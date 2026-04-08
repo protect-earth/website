bc80200431764adfe1f830e8cf475dc0ed043958
Alex Sexton <alex@alexsexton.cc>
2026-04-08 10:32:42 -0300

    add isSameDay helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -5,3 +5,25 @@ export const toKebabCase = (str: string): string =>
 		.replace(/\s+/g, '-')
 		.replace(/--+/g, '-')
 		.trim();
+
+export const isSameDay = (start: any, end: any): boolean => {
+	let starts, ends;
+	let day = false;
+
+	starts = start.toLocaleString('en-GB', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
+
+	ends = end.toLocaleString('en-GB', {
+		day: 'numeric',
+		month: 'long',
+		year: 'numeric',
+	});
+
+	if (starts === ends) {
+		day = true;
+	}
+	return day;
+};
