8fab49a96c5d4d09b0009c7cea91742ee76e4fbe
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2026-05-07 12:42:02 +0100

    feat: support mdx and add embed component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 import netlify from '@astrojs/netlify';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.protect.earth',
 	output: 'static',
 	adapter: netlify(),
+
 	vite: {
 		plugins: [yaml()],
 	},
+
+	integrations: [mdx()],
 });
--- src/config.ts
+++ src/config.ts
@@ -16,9 +16,9 @@ export const siteConfig = {
 	},
 
 	achievements: {
-		treesPlanted: 157453,
+		treesPlanted: 158178,
 		acresRestored: 400,
-		acresUnderManagement: 180,
+		acresUnderManagement: 248,
 	},
 
 	// Sites to ignore from API data
